318d919c6c71c5e784f38fcf1a20f3c5e09e48df
Alan Villarreal <nydhoggr@protonmail.com>
2026-01-06 14:28:18 -0600

    Element regression counter solution for checking if value bigger than average

---
+++ .idea/src/SoftwareEngineerPrep/ElementRegressionCounter.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+class ElementRegressionCounter {
+    public static int countResponseTimeRegressions(List<Integer> responseTimes) {
+
+        // Constraint check:
+        if (responseTimes.isEmpty()){
+            return 0;
+        }
+
+        int average = 0;
+        int dayCount = 0;
+
+        for(int i = 0; i < responseTimes.size(); i++) {
+
+            // Constraint check : handles invalid data type
+            if (responseTimes.get(i) < 1) {
+
+                return 0;
+            }
+
+            if (i == 0) {
+                average += responseTimes.get(i);
+                continue;
+            }
+
+
+            if(responseTimes.get(i) > average) {
+                dayCount++;
+            }
+
+            average += responseTimes.get(i);
+            average /= i + 1;
+
+        }
+
+        return dayCount;
+    }
+}
+
